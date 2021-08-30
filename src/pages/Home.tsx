cac9c5170c0a868c57c3a3b37e4a7874bd46d708
Trevor Brown <trevorbrown.dev@gmail.com>
2021-08-30 12:35:13 -0400

    cleaning repo

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { BingoCard } from "../components/BingoCard"
-
-export const Home: React.FC = () => {
-    return (
-        <div className="home">
-            <BingoCard width={3} height={3} cellContent={["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]} />
-        </div>
-    )
-}
